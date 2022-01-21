6f0a86ceb8a063e084670b0c171eb6fe7b609953
A1 <A1@LAPTOP-51URUTK2>
2022-01-21 18:50:09 +0530

    BY MS

---
+++ Git_dec_18/src/ex_1/Test.java
@@ -0,0 +1,13 @@
+package ex_1;
+
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello");
+		System.out.println("hello1");
+		System.out.println("hello2");
+		
+	}
+
+}
---
+++ Git_dec_18/src/ex_3/Test2.java
@@ -0,0 +1,24 @@
+package ex_3;
+
+public class Test2 
+{
+	public static void main(String[] args) 
+	{
+		Test2 t = new Test2();
+		t.m1();
+		t.m2();
+		
+		System.out.println("Main Class");
+	}
+	
+	public void m1()
+	{
+		System.out.println("M1 Method");
+	}
+
+	public void m2()
+	{
+		System.out.println("M2 Method");
+	}
+
+}
